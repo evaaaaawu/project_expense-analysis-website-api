96a25565adf9f5d441bc352e1c7993c0bc9c3856
evaaaaawu <yiju.wu1@gmail.com>
2024-03-29 17:42:33 +0800

    feat: add error handler

--- app.js
+++ app.js
@@ -5,18 +5,21 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-//TODO const cors = require('cors')
+const cors = require('cors')
 
 const routes = require('./routes')
 require('./config/mongoose')
+
+const { generalErrorHandler } = require('./middleware/error-handler')
 
 const app = express() //建構應用程式伺服器
 const port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 app.use(methodOverride('_method'))
-//TODO app.use(cors())
+app.use(cors()) // 這會允許所有跨域請求
 app.use(routes)
+app.use(generalErrorHandler) // 在所有路由之後使用錯誤處理中間件
 
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
--- services/user-services.js
+++ services/user-services.js
@@ -1,13 +1,14 @@
 const User = require('../models/user')
+const createError = require('http-errors')
 
 const userServices = {
   signUp: async (req, cb) => {
     try {
       const { email, password, checkPassword } = req.body
-      if ( !email || !password || !checkPassword) throw new Error('所有欄位皆為必填！')
+      if ( !email || !password || !checkPassword) throw createError(400, '所有欄位皆為必填！')
       const user = await User.findOne({ email })
-      if (user) throw new Error('信箱已存在！')
-      if (password !== checkPassword) throw new Error('密碼與確認密碼不一致！')
+      if (user) throw createError(400, '信箱已存在！')
+      if (password !== checkPassword) throw createError(400, '密碼與確認密碼不一致！')
       // const salt = bcrypt.genSaltSync(10)
       // const hash = bcrypt.hashSync(password, salt)
       const newUser = await User.create({
