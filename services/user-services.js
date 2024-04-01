26c23501615f58359971df8377245ddf44fc5c1a
evaaaaawu <yiju.wu1@gmail.com>
2024-04-01 16:49:29 +0800

    feat: add and use bcryptjs

--- services/user-services.js
+++ services/user-services.js
@@ -1,26 +1,26 @@
 const User = require('../models/user')
 const createError = require('http-errors')
+const bcrypt = require('bcryptjs')
 
 const userServices = {
   signUp: async (req, cb) => {
     try {
       const { email, password, checkPassword } = req.body
-      if ( !email || !password || !checkPassword) throw createError(400, '所有欄位皆為必填！')
+      if ( !email || !password || !checkPassword) throw createError(400, 'All information is required.')
       const user = await User.findOne({ email })
-      if (user) throw createError(400, '信箱已存在！')
-      if (password !== checkPassword) throw createError(400, '密碼與確認密碼不一致！')
-      // const salt = bcrypt.genSaltSync(10)
-      // const hash = bcrypt.hashSync(password, salt)
+      if (user) throw createError(400, 'This email address has already been registered.')
+      if (password !== checkPassword) throw createError(400, 'Password and confirmation password do not match.')
+      const salt = bcrypt.genSaltSync(10)
+      const hash = bcrypt.hashSync(password, salt)
       const newUser = await User.create({
         email,
-        password,
-        // password: hash
+        password: hash
       })
       const userData = newUser.toJSON()
       delete userData.password
       cb(null, {
           status: 'success',
-          message: '註冊成功！',
+          message: 'Registration successful!',
           user: userData
       })
     } catch (err) {
